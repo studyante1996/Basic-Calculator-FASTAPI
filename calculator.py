c8818c10e8f3895b58152be0f13bd771d1462045
studyante1996 <62179790+studyante1996@users.noreply.github.com>
2024-10-27 23:09:11 +0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+@app.get("/eval")
+def calculate(
+    Operator: str = Query(..., description="Choose to Calculate type: add |minus | times | devide"),
+    firstDigit: float = Query(..., description="First Digit"),
+    secondDigit: float = Query(..., description="Second Digit")
+):
+    if Operator == "add":
+        result = firstDigit + secondDigit
+    elif Operator == "minus":
+        result = firstDigit - secondDigit
+    elif Operator == "times":
+        result = firstDigit * secondDigit
+    elif Operator == "divide":
+        if secondDigit == 0:
+            return {"error": "! divide secondDigity zero"}
+        result = firstDigit / secondDigit
+    else:
+        return {"error": "Invalid!!! Just type add, minus, times, division"}
+    
+    return {"Operator": Operator, "a": firstDigit, "secondDigit": secondDigit, "result": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
