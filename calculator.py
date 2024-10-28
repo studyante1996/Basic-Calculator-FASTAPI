084dc9de9b4675ff21384a32a3b172a723ddc955
studyante1996 <62179790+studyante1996@users.noreply.github.com>
2024-10-28 11:57:23 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -25,5 +25,5 @@ def calculate(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=10000 , reload=True)
 
